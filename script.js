274846038489892ba500459560e3a51cd0a2f22b
AJ Wint <ajwint.cs@gmail.com>
2024-03-30 12:21:52 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showPopup() {
+  document.getElementById("prototypePopup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+  document.getElementById("prototypePopup").style.display = "none";
+}
